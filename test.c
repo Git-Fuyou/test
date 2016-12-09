fd49ee139e8689a5beb3228c68ace867d73f7a93
Fuyou-laptop <ruboss@126.com>
2016-12-09 22:02:12 +0800

    编码统一

--- test.c
+++ test.c
@@ -2,4 +2,4 @@ creat file test.c
 add one line at 16:27.
 add by fuyou at 21:58 PM,use the desktop PC.
 add bu fuyou at 11:55 AM,through the public PC in KWT.inc.
-
+全部使用UTF-8编码就不会乱码，Keil设为UTF-8的不好之处在于串口printf时，串口助手不支持UTF-8编码的话，显示就会乱码。
